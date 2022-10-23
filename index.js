89f2e283cc249ea6f247afc8013b7ff2d6ac625e
MrTlyer <63327598+MrTlyer@users.noreply.github.com>
2022-10-23 10:46:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  *  自定义网站配置 
  */
 const config = {
-  title: "自定义导航",                 //write your website title
-  subtitle: "Cloudflare Workers Dir", //write your website subtitle
+  title: "导航",                 //write your website title
+  subtitle: "", //write your website subtitle
   logo_icon: "sitemap",               //select your logo by semantic-ui icon (you can get more msg in:https://semantic-ui.com/elements/icon.html)
   hitokoto: true,                     //use hitokoto or not
   search:true,                        //enable search function
@@ -11,6 +11,10 @@ const config = {
     {
       name:"百 度",
       template:"https://www.baidu.com/s?wd=$s"
+    },
+    {
+      name:"头 条",
+      template:"https://so.toutiao.com/search?dvpf=pc&source=sug&keyword=$s"
     },
     {
       name:"谷 歌",
@@ -27,13 +31,13 @@ const config = {
   ],
   selling_ads: true,                  //Selling your domain or not.(turning on may be helpful for selling this domain by showing some ads.)
   sell_info:{
-    domain:"example.com",
-    price:500,                        //domain price
+    domain:"blog.ntnas.top",
+    price:0,                        //domain price
     mon_unit:"yen sign",              //monetary unit 
     contact:[                         //how to contact you
       {
         type:"envelope",               //contact type ("weixin","qq","telegram plane","envelope" or "phone")
-        content:"info@example.com"
+        content:"echoding@163.com"
       }
     ]                        
   },
@@ -100,6 +104,7 @@ async function handleRequest(request) {
       'content-type': 'text/html;charset=UTF-8',
     },
   }
+  
   return new Response(renderHTML(renderIndex(),config.selling_ads? renderSeller() :null), init);
 }
 addEventListener('fetch', event => {
@@ -112,10 +117,10 @@ addEventListener('fetch', event => {
 function getFavicon(url){
   if(url.match(/https{0,1}:\/\//)){
     //return "https://ui-avatars.com/api/?bold=true&size=36&background=0D8ABC&color=fff&rounded=true&name=" + url.split('//')[1];
-    return "https://www.google.cn/s2/favicons?sz=64&domain_url=" + url;
+    return "https://icon.horse/icon/" + url.split('//')[1];
   }else{
     //return "https://ui-avatars.com/api/?bold=true&size=36&background=0D8ABC&color=fff&rounded=true&name=" + url;
-    return "https://www.google.cn/s2/favicons?sz=64&domain_url=http://" + url;
+    return "https://icon.horse/icon/" + url;
   } 
 }
 
@@ -124,7 +129,7 @@ function getFavicon(url){
  */
 
 function renderIndex(){
-  const footer = el('footer',[],el('div',['class="footer"'],'Powered by' + el('a',['class="ui label"','href="https://github.com/sleepwood/cf-worker-dir"','target="_blank"'],el('i',['class="github icon"'],"") + 'Cf-Worker-Dir') + ' &copy; Base on ' + el('a',['class="ui label"'],el('i',['class="balance scale icon"'],"") + 'MIT License')));
+  const footer = el('footer',[],el('div',['class="footer"'],'Powered by' + el('a',['class="ui label"','href="https://ntnas.top:82/typecho"','target="_blank"'],el('i',['class="github icon"'],"") + 'Blog') + ' &copy; Base on ' + el('a',['class="ui label"'],el('i',['class="balance scale icon"'],"") + 'MIT License')));
   return renderHeader() + renderMain() + footer;
 }
 
@@ -141,7 +146,7 @@ function renderHeader(){
     }
   }).join(""))
   var input = el('div',['class="ui left corner labeled right icon fluid large input"'],el('div',['class="ui left corner label"'],el('img',['id="search-fav"','class="left floated avatar ui image"','src="https://www.baidu.com/favicon.ico"'],"")) + el('input',['id="searchinput"','type="search"','placeholder="搜索你想要知道的……"','autocomplete="off"'],"") + el('i',['class="inverted circular search link icon"'],""));
-  return el('header',[],el('div',['id="head"','class="ui inverted vertical masthead center aligned segment"'],(config.hitokoto ? el('div',['id="nav"','class="ui container"'],nav) : "") + el('div',['id="title"','class="ui text container"'],title + (config.search ? input + menu :"") + `${config.selling_ads ? '<div><a id="menubtn" class="red ui icon inverted button"><i class="heart icon"></i> 喜欢此域名 </a></div>' : ''}`)))
+  return el('header',[],el('div',['id="head"','class="ui inverted vertical masthead center aligned segment"'],(config.hitokoto ? el('div',['id="nav"','class="ui container"'],nav) : "") + el('div',['id="title"','class="ui text container"'],title + (config.search ? input + menu :"") + `${config.selling_ads ? '<div><a id="menubtn" class="red ui icon inverted button"><i class="heart icon"></i>  </a></div>' : ''}`)))
 }
 
 function renderMain() {
@@ -161,7 +166,7 @@ function renderMain() {
 
 function renderSeller() {
   const item = (type,content) => el('div',['class="item"'],el('i',[`class="${type} icon"`],"") + el('div',['class="content"'],content));
-  var title = el('h1',['class="ui yellow dividing header"'],el('i',['class="gem outline icon"'],"") + el('div',['class="content"'],config.sell_info.domain + ' 正在出售'));
+  var title = el('h1',['class="ui yellow dividing header"'],el('i',['class="gem outline icon"'],"") + el('div',['class="content"'],config.sell_info.domain + ' '));
   var action = el('div',['class="actions"'],el('div',['class="ui basic cancel inverted button"'],el('i',['class="reply icon"'],"") + '返回'));
 
   var contact = config.sell_info.contact.map((list) => {
@@ -186,6 +191,8 @@ function renderHTML(index,seller) {
       <link href="https://cdn.jsdelivr.net/gh/sleepwood/cf-worker-dir@0.1.1/style.css" rel="stylesheet">
       <script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js"></script>
       <script src="https://cdn.jsdelivr.net/npm/semantic-ui-css@2.4.1/semantic.min.js"></script>
+      <link rel="shortcut icon" href="https://ntnas.top:82/favicon.ico" />
+      <link rel="bookmark" href="https://ntnas.top:82/favicon.ico" type="image/x-icon"　/>
   </head>
   <body>
     ${index}
@@ -195,7 +202,7 @@ function renderHTML(index,seller) {
       $('#sengine a').on('click', function (e) {
         $('#sengine a.active').toggleClass('active');
         $(e.target).toggleClass('active');
-        $('#search-fav').attr('src',$(e.target).data('url').match(`+/https{0,1}:\/\/\S+\//+`)[0] + '/favicon.ico') ;
+        $('#search-fav').attr('src',$(e.target).data('url').match(`+/https{0,1}:\/\/\S+\//+`)[0] + 'favicon.ico') ;
       });
       $('.search').on('click', function (e) {
           var url = $('#sengine a.active').data('url');
@@ -214,6 +221,5 @@ function renderHTML(index,seller) {
       });
     </script>
   </body>
-
   </html>`
 }
